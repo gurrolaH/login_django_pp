9f1b65549d3456a9ed298b53f83058eb1cbe25b0
hectorg gurrola <bhm.hector.gm@gmail.com>
2026-08-13 12:11:56 -0600

    feat (proveedor): implementación modelo y migración de proveedor.

--- app/proveedor/admin.py
+++ app/proveedor/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Proveedor
 
-# Register your models here.
+# Para poder administrar desde admin
+
+@admin.register(Proveedor)
+class ProveedorAdmin(admin.ModelAdmin):
+    list_display = (
+        'id_proveedor',
+        'nombre',
+        'telefono',
+        'email',
+        'activo',
+    )
+
+    search_fields = (
+        'nombre',
+        'email',
+        'telefono',
+    )
+
+    list_filter = (
+        'activo',
+    )
--- app/proveedor/models.py
+++ app/proveedor/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Proveedor(models.Model):
+
+    # Llave primaria:
+    id_proveedor = models.BigAutoField(primary_key=True)
+
+    nombre = models.CharField(max_length=150)
+
+    telefono = models.CharField(max_length=20, blank=True)
+    email = models.EmailField(max_length=150, blank=True)
+    direccion = models.CharField(max_length=255, blank=True)
+    contacto = models.CharField(max_length=100, blank=True)
+
+    activo = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'proveedor'
+        ordering = ['nombre']
+
+    def __str__(self):
+        return self.nombre
