5578a025f2ce4ab6ce509a953054285099058833
hectorg gurrola <bhm.hector.gm@gmail.com>
2026-08-14 10:41:23 -0600

    feat (READ): Consultar los empleados en existencia.

--- app/empleado/urls.py
+++ app/empleado/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
+app_name = 'empleado'
 
+urlpatterns = [
+    path('listar-empleados/', views.listar_empleados_view, name='lista'),
+    #path('registrar-empleados', views.registrar_empleados_view, name='crear'),
 ]
--- app/empleado/views.py
+++ app/empleado/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Empleado
 
-# Create your views here.
+
+
+# Esta vista nos permitirá listar a todos los empleados:
+@login_required
+def listar_empleados_view(request):
+
+    # Obtener lso datos del modelo sin filtros.
+    empleados = Empleado.objects.all()
+
+    # Pasar los datos a un context.
+    context = {
+        'empleados': empleados
+    }
+
+    # Hacer la el reenderizado.
+    return render(request, 'empleados/lista-empleados.html', context)
